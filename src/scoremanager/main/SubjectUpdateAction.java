5fdaccf29ad25390fe28fbaf7aa32f202ece3d41
airisato <tky2302081@stu.o-hara.ac.jp>
2024-05-07 11:28:56 +0900

    5/7

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -15,13 +15,9 @@ public class SubjectUpdateAction extends Action {
 	public void execute(HttpServletRequest req, HttpServletResponse res)throws Exception {
 
 		HttpSession session = req.getSession();//セッション
-
 		Teacher teacher = (Teacher)session.getAttribute("user");
-
 		String cd = req.getParameter("cd");
-
 		SubjectDao p=new SubjectDao();
-
 		Subject a = p.get(cd, teacher.getSchool());
 
 		req.setAttribute("subject", a);
