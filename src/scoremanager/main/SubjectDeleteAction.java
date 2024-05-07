fd450d0ea2be7a17d7852c3aa89ddbcffcde3cfd
airisato <tky2302081@stu.o-hara.ac.jp>
2024-05-07 11:35:36 +0900

    5/7

--- src/scoremanager/main/SubjectDeleteAction.java
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -14,17 +14,11 @@ public class SubjectDeleteAction extends Action {
 
 		@Override
 		public void execute(HttpServletRequest req, HttpServletResponse res)throws Exception {
-
 			HttpSession session = req.getSession();//セッション
-
 			Teacher teacher = (Teacher)session.getAttribute("user");
-
 			String cd = req.getParameter("cd");
-
 			SubjectDao p=new SubjectDao();
-
 			Subject a = p.get(cd, teacher.getSchool());
-
 			req.setAttribute("subject", a);
 
 
