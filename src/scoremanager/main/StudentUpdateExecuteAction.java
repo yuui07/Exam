43b4c731437a6539ba1f63e4f77625daacefd9a3
AzureSky235 <tky2302113@stu.o-hara.ac.jp>
2024-05-08 09:56:11 +0900

    a

--- src/scoremanager/main/StudentUpdateExecuteAction.java
+++ src/scoremanager/main/StudentUpdateExecuteAction.java
@@ -107,4 +107,4 @@ public class StudentUpdateExecuteAction extends Action {
 //		//JSPにフォワード 7
 //		request.getRequestDispatcher("student_list.jsp").forward(request,response);
 
-コンテキスト メニューあり
+//コンテキスト メニューあり
