53bec3b4d1b55807d6bdd7e0c3d4479b7205f490
AzureSky235 <tky2302113@stu.o-hara.ac.jp>
2024-05-09 10:29:09 +0900

    a

--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -45,7 +45,7 @@ public class TestRegistAction extends Action {
         List<Integer> entYearList = new ArrayList<>();
 
         // クラス番号一覧取得
-        List<String> classNumList = classNumDao.filter();
+        List<String> classNumList = classNumDao.filter(null);
 
         // リクエスト属性にセット
         request.setAttribute("f1", entYear);
